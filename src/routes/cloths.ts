543806b6d3486c59a51f72628a1d96f39489eea6
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-06-10 16:49:28 -0300

    feat: add cloth routes (GET, POST, PATCH, DELETE)

--- src/routes/cloths.ts
+++ src/routes/cloths.ts
@@ -1,9 +1,12 @@
+import { addCloth, getAllCloths, getCloth, updateCloth,deleteCloth } from "../controllers/cloths";
 import express from "express";
 
 const router = express.Router();
 
-router.get('/', (req,res) => {
-    res.json({msg: "oi"})
-})
+router.get("/", getAllCloths);
+router.get("/:id", getCloth);
+router.post("/", addCloth);
+router.patch("/:id", updateCloth);
+router.delete("/:id", deleteCloth);
 
 export default router;

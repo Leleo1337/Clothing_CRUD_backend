1507d34ad2f4cc131314d6c54d51c1c3c217acf7
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-06-10 16:43:47 -0300

    chore: removed dist files

--- dist/model/cloth.js
+++ dist/model/cloth.js
@@ -1,2 +1,29 @@
-export {};
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = __importDefault(require("mongoose"));
+const clothSchema = new mongoose_1.default.Schema({
+    name: {
+        type: String,
+        required: [true, "Por favor, insira um nome"],
+        trim: true,
+    },
+    quantity: {
+        type: Number,
+        required: [true, "Por favor, insira uma quantity"],
+    },
+    price: {
+        type: Number,
+        required: [true, "Por favor, insira um preço"],
+    },
+    size: {
+        type: String,
+        required: [true, "Por favor insira um tamanho ( PP | P  | M | G | GG | XG | XGG | EG | EGG )"],
+        enum: ["PP", "P", "M", "G", "GG", "XG", "XGG", "EG", "EGG"],
+    },
+});
+const Cloth = mongoose_1.default.model("cloths", clothSchema);
+exports.default = Cloth;
 //# sourceMappingURL=cloth.js.map
--- dist/server.js
+++ dist/server.js
@@ -1,16 +1,21 @@
-import { exit } from "process";
-import app from "./app";
-import connectDB from "./db/connect";
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const process_1 = require("process");
+const app_1 = __importDefault(require("./app"));
+const connect_1 = __importDefault(require("./db/connect"));
 const MONGO_URI = process.env.MONGO_URI;
 const PORT = process.env.PORT;
 async function start() {
     try {
-        if (!MONGO_URI || PORT) {
+        if (!MONGO_URI || !PORT) {
             console.log("Please fill all environment variables");
-            exit(1);
+            (0, process_1.exit)(1);
         }
-        await connectDB(MONGO_URI);
-        app.listen(PORT, () => console.log(`app listening on port ${PORT}`));
+        await (0, connect_1.default)(MONGO_URI);
+        app_1.default.listen(PORT, () => console.log(`app listening on port ${PORT}`));
     }
     catch (error) {
         console.log(error);

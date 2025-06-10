da78eea37677bf76711d1d64ac1c182866dc7287
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-06-10 16:45:42 -0300

    feat: added errors handling middlewares and utils

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,23 @@
-import { configDotenv } from "dotenv";
 import express from "express";
-import cloths from "./routes/cloths";
+import cors from 'cors'
+import { configDotenv } from "dotenv";
+import cloths from './routes/cloths'
+import errorHandlerMiddleware from "./middlewares/errorHandler";
+import routeNotFound from "./middlewares/routeNotFound";
 
 configDotenv();
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send('Clothing api, please call /api/v1/cloths')
-});
+// middlewares
+app.use(express.json())
+app.use(cors())
 
+// routes
 app.use("/api/v1/cloths", cloths);
+
+// errors handlers middlewares
+app.use(errorHandlerMiddleware)
+app.use(routeNotFound)
 
 export default app;

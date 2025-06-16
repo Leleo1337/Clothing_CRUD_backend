9ae1a28ad4269e6bfc70dd5d06606ed7b0d7d569
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-06-16 19:12:43 -0300

    refactor: saltRolls is now optional + removed bad request error

--- src/utils/bcrypt.ts
+++ src/utils/bcrypt.ts
@@ -1,17 +1,10 @@
 import bcrypt from "bcrypt";
-import BadRequest from "errors/badRequest";
 
-export async function hashValue(val: string, saltRolls: number) {
+export async function hashValue(val: string, saltRolls?: number) {
    const rounds = saltRolls || 10;
    return bcrypt.hash(val, rounds);
 }
 
 export async function compareValue(val: string, hashedVal: string) {
-   const matches = await bcrypt.compare(val, hashedVal);
-
-   if (!matches) {
-      throw new BadRequest("Invalid password");
-   }
-
-   return true;
+   return await bcrypt.compare(val, hashedVal)
 }

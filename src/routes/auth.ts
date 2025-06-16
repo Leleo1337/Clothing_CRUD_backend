5377ad588d2b8b17684ec13ae456053222358624
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-06-16 19:10:54 -0300

    feat: add login route
    also removed register type ( it was an attempt to fix the methods bug )

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,9 +1,10 @@
-import { register } from "../controllers/auth";
+import { login, register } from "../controllers/auth";
 import express from "express";
 
 const router = express.Router();
 
-router.post("/register", register as any);
+router.post("/register", register);
+router.post("/login", login);
 
 
 export default router;
